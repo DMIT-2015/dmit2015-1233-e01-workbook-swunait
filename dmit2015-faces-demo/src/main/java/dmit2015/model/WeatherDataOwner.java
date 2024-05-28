7832d87e68545a40901c73dcfb27355cf6b89760
Sam Wu <swu@nait.ca>
2024-05-27 18:23:19 -0600

    Added FireAuth CRUD example with PrimeFaces.

---
+++ dmit2015-faces-demo/src/main/java/dmit2015/model/WeatherDataOwner.java
@@ -0,0 +1,72 @@
+package dmit2015.model;
+
+import java.time.LocalDate;
+
+/**
+ * This class tracks historical weather data for a city.
+ *
+ * @version 2024.05.17
+ */
+public class WeatherDataOwner {
+
+    private String name;
+
+    private String city;
+
+    private LocalDate date;
+
+    private int temperatureCelsius;
+
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public int getTemperatureCelsius() {
+        return temperatureCelsius;
+    }
+
+    public void setTemperatureCelsius(int temperatureCelsius) {
+        this.temperatureCelsius = temperatureCelsius;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "WeatherData{" +
+                "name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", date=" + date +
+                ", temperatureCelsius=" + temperatureCelsius +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
